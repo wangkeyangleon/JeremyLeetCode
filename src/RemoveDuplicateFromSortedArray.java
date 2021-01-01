a697f1065208664a52951b5da759cdca95e84fab
Keyang Wang <kwan0058@student.monash.edu>
2021-01-01 21:44:57 +1100

    remove the duplicated number in the array

---
+++ src/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,8 @@
+/**
+ * @program: LeetCode
+ * @description: Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.  Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * @author: Keyang Wang
+ * @create: 2021-01-01 12:36
+ **/
+public class RemoveDuplicateFromSortedArray {
+}
