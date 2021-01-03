92217515f4c964e08dc0a508407e4c31c7d8de74
Keyang Wang <kwan0058@student.monash.edu>
2021-01-03 18:41:13 +1100

    the question has been solved

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,8 +1,19 @@
 /**
  * @program: LeetCode
- * @description: You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * @description: You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * Example:
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
  * @author: Keyang Wang
  * @create: 2020-12-30 19:50
  **/
 public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+
+    }
 }
--- src/RemoveDuplicateFromSortedArray.java
+++ src/RemoveDuplicateFromSortedArray.java
@@ -1,8 +1,57 @@
 /**
  * @program: LeetCode
- * @description: Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.  Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * @description: 26
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * Example 1:
+ * <p>
+ * Given nums = [1,1,2],
+ * <p>
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * <p>
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ * <p>
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * <p>
+ * Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+ * <p>
+ * It doesn't matter what values are set beyond the returned length.
  * @author: Keyang Wang
  * @create: 2021-01-01 12:36
  **/
+
 public class RemoveDuplicateFromSortedArray {
+
+    public int remove(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+        int a = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[a] != nums[i]) {
+                a++;
+                nums[a] = nums[i];
+            }
+        }
+        System.out.println("++++++++++");
+        for (int i : nums
+        ) {
+            System.out.println(i);
+        }
+        System.out.println("++++++++++");
+        return a + 1;
+
+
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicateFromSortedArray removeDuplicateFromSortedArray = new RemoveDuplicateFromSortedArray();
+        int[] nums = {1, 1, 2};
+        System.out.println(removeDuplicateFromSortedArray.remove(nums));
+        int[] num2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicateFromSortedArray.remove(num2));
+
+    }
+
 }
