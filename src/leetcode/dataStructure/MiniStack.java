c493ac74e00428ad5d859f1cdce52829c523f15e
Keyang Wang <kwan0058@student.monash.edu>
2021-01-08 23:43:03 +1100

    this version is not a good method to solve the problem

--- src/leetcode/dataStructure/MiniStack.java
+++ src/leetcode/dataStructure/MiniStack.java
@@ -1,25 +1,27 @@
 package leetcode.dataStructure;
 
+import java.util.Deque;
+
 /**
  * @program: LeetCode
  * @description: 155
- *
+ * <p>
  * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
- *
+ * <p>
  * push(x) -- Push element x onto stack.
  * pop() -- Removes the element on top of the stack.
  * top() -- Get the top element.
  * getMin() -- Retrieve the minimum element in the stack.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input
  * ["MinStack","push","push","push","getMin","pop","top","getMin"]
  * [[],[-2],[0],[-3],[],[],[],[]]
- *
+ * <p>
  * Output
  * [null,null,null,null,-3,null,0,-2]
- *
+ * <p>
  * Explanation
  * MinStack minStack = new MinStack();
  * minStack.push(-2);
@@ -29,31 +31,78 @@ package leetcode.dataStructure;
  * minStack.pop();
  * minStack.top();    // return 0
  * minStack.getMin(); // return -2
- *
  * @author: Keyang Wang
  * @create: 2021-01-07 21:58
  **/
 public class MiniStack {
+    private int maxsize = 100;
+    private int[] element;
+    private int size;
+    private int min;
 
-    /** initialize your data structure here. */
+    /**
+     * initialize your data structure here.
+     */
     public MiniStack() {
-
+        element = new int[maxsize];
+        min = Integer.MAX_VALUE;
+        size = 0;
     }
 
     public void push(int x) {
-
+        if (size >= maxsize) {
+            return;
+        } else {
+            size++;
+            element[size-1] = x;
+        }
+        if (x <= min)
+            min = x;
     }
 
     public void pop() {
+        if (min==element[size-1]){
+            min = Integer.MAX_VALUE;
+            for (int i = 0; i < size-1; i++) {
+                if (min>= element[i])
+                    min = element[i];
+            }
+            element[size-1]=0;
+            size--;
+        }else
+        {
+            element[size-1] = 0;
+            size --;
+        }
 
     }
 
     public int top() {
-        return 0;
+        if (size == 0) {
+            System.out.println("the stack is empty");
+            return 0;
+        } else {
+            return element[size-1];
+        }
+
     }
 
     public int getMin() {
-        return 0;
+        return min;
+    }
+
+    public static void main(String[] args) {
+       int a = -2;
+       int b = -3;
+       MiniStack miniStack = new MiniStack();
+       miniStack.push(a);
+       miniStack.push(b);
+        System.out.println(miniStack.getMin());
+        System.out.println("top"+miniStack.top());
+        System.out.println("+++++++++++++++++++");
+        miniStack.pop();
+        System.out.println("top"+miniStack.top());
+        System.out.println("min"+miniStack.getMin());
     }
 }
 
