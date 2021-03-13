a42a47154c46642200373b01cd32a1437b98da97
Keyang Wang <kwan0058@student.monash.edu>
2021-03-13 19:42:41 +1100

    rewrite select sort

--- src/leetcode/algorithm/SelectSort.java
+++ src/leetcode/algorithm/SelectSort.java
@@ -9,25 +9,42 @@ import java.util.Arrays;
  * @create: 2021-02-27 17:30
  **/
 public class SelectSort {
-    private int[] selectSort(int arr[]){
-        if (arr==null){
+    private int[] selectSort(int arr[]) {
+        if (arr == null) {
             return null;
         }
-        for (int i = 0; i <arr.length ; i++) {
-            for (int j = i; j <arr.length ; j++) {
-                if (arr[j] < arr[i]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
-                    arr[j] =temp;
+                    arr[j] = temp;
                 }
             }
         }
         return arr;
     }
 
+    private int[] selectSort2(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
         SelectSort sort = new SelectSort();
-        int arr[] = {11,1,2,5,11,6,9};
-        System.out.println(Arrays.toString(sort.selectSort(arr)));
+        int arr[] = {11, 1, 2, 5, 11, 6, 9};
+        System.out.println(Arrays.toString(sort.selectSort2(arr)));
     }
 }
