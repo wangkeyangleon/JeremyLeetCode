4115ca70ae36866931415f9d5eb6c9a16ef7db69
Keyang Wang <kwan0058@student.monash.edu>
2021-07-25 15:54:33 +1000

    replace space

--- src/leetcode/dataStructure/ReplaceSpace.java
+++ src/leetcode/dataStructure/ReplaceSpace.java
@@ -11,26 +11,63 @@ public class ReplaceSpace {
 
     /**
      * brute force
+     *
      * @param s
      * @return
      */
     public String replaceSpace(String s) {
-        if (s == null || s.isEmpty()){
+        if (s == null || s.isEmpty()) {
             return s;
         }
         //change the string to char array
-        char[] c = s.toCharArray();
-        for (char c1: c) {
-            if (c1 == ' '){
-                
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (char c : chars
+        ) {
+            if (c == ' ') {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(c);
             }
         }
-        return "";
+        return stringBuilder.toString();
     }
+
+    public String replaceSpace2(String s) {
+        //check whether the string is null or empty
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        int count = 0; //calculate number of spaces
+        for (char c : chars
+        ) {
+            if (c == ' ') {
+                count++;
+            }
+        }
+        //define a new char to save the s
+        char[] newChar = new char[chars.length + count*2];
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' '){
+                newChar [index++] = '%';
+                newChar[index++] = '2';
+                newChar[index++] ='0';
+            }else {
+                newChar[index++] = s.charAt(i);
+            }
+        }
+
+        return new String(newChar,0,index);
+    }
+
 
     public static void main(String[] args) {
         String s = "We are happy.";
         ReplaceSpace replaceSpace = new ReplaceSpace();
+        System.out.println(replaceSpace.replaceSpace(s));
+        System.out.println(replaceSpace.replaceSpace2(s));
 
     }
 }
